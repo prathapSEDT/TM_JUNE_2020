6fa48a36942bebe21b3fd97a785dff078d96ceb1
prathap <prathap.ufttest@gmail.com>
2020-08-07 10:34:18 +0530

    added all the notes

--- src/test/java/database/JDBC.java
+++ src/test/java/database/JDBC.java
@@ -1,2 +1,16 @@
-package database;public class JDBC {
+package database;
+
+
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBC {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+Configuration configuration=new Configuration();
+
+    }
 }
